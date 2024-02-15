69f4d73d7fafbfe40b76d3e727c275714104a670
Yashvardhan Kukreja <yash.kukreja.98@gmail.com>
2024-02-14 20:18:02 -0500

    init commit
    Signed-off-by: Yashvardhan Kukreja <yash.kukreja.98@gmail.com>

---
+++ cmd/serve/cmd.go
@@ -0,0 +1,44 @@
+package serve
+
+import (
+	"bitbucket.com/finease/backend/pkg/environment"
+	"bitbucket.com/finease/backend/pkg/routers"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/golang/glog"
+	"github.com/spf13/cobra"
+	"log"
+)
+
+func NewServeCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "serve",
+		Short: "Serve the finease-backend",
+		Long:  "Serve the finease-backend.",
+		Run:   runServe,
+	}
+
+	if err := environment.Environment().Setup(cmd.PersistentFlags()); err != nil {
+		glog.Fatalf("Unable to setup the environment config: '%v'", err.Error())
+	}
+
+	return cmd
+}
+
+func runServe(cmd *cobra.Command, args []string) {
+	env := environment.Environment()
+	server := gin.New()
+
+	server.NoRoute(func(c *gin.Context) {
+		c.JSON(404, gin.H{"code": "Not found", "message": "Page not found", "env": env.ServerConfig.EnvName})
+	})
+
+	server.Use(gin.Recovery())
+
+	glog.Infof("server running in %s mode", env.ServerConfig.EnvName)
+
+	routers.SetupRouter(server)
+
+	host, port := env.ServerConfig.ListenAddress, env.ServerConfig.ListenPort
+	log.Fatal(server.Run(fmt.Sprintf("%s:%d", host, port)))
+}
