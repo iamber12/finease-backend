4de041958a4f0159ee92d593726edab3171715a6
VERMA\verma <a23verma@uwaterloo.ca>
2024-02-19 11:30:04 -0500

    FIN-12 - Environment based integration with the server

--- cmd/serve/main.go
+++ cmd/serve/main.go
@@ -1,38 +1,42 @@
 package serve
 
-import (  
-	"bitbucket.com/finease/backend/pkg/environment"  
-	"bitbucket.com/finease/backend/pkg/routers"  
-	"fmt"  
-	"github.com/gin-gonic/gin"  
-	"github.com/golang/glog"  
-	"github.com/spf13/cobra"  
-	"log"  
-	)  
-	  
-	func NewServeCommand() *cobra.Command {  
-		cmd := &cobra.Command{  
-			Use: "serve",  
-			Short: "Serve the finease-backend",  
-			Long: "Serve the finease-backend.",  
-			Run: runServe,  
-		}  
-		
-		return cmd  
-	}  
-	  
-	func runServe(cmd *cobra.Command, args []string) {  
-		server := gin.New()  
-		
-		server.NoRoute(func(c *gin.Context) {  
-			c.JSON(404, gin.H{"code": "Not found", "message": "Page not found"})  
-		})  
-		
-		server.Use(gin.Recovery())  
-		
-		glog.Infof("server running")  
-		
-		host, port := "localhost", 8000
-		err := server.Run(fmt.Sprintf("%s:%d", host, port))  
-		log.Fatal(err)  
+import (
+	"bitbucket.com/finease/backend/pkg/environment"
+	"bitbucket.com/finease/backend/pkg/routers"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/golang/glog"
+	"github.com/spf13/cobra"
+	"log"
+)
+
+func NewServeCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "serve",
+		Short: "Serve the finease-backend",
+		Long:  "Serve the finease-backend.",
+		Run:   runServe,
 	}
+
+	if err := environment.Environment().Setup(cmd.PersistentFlags()); err != nil {
+		glog.Fatalf("Unable to setup the environment config: '%v'", err.Error())
+	}
+
+	return cmd
+}
+
+func runServe(cmd *cobra.Command, args []string) {
+	env := environment.Environment()
+	server := gin.New()
+
+	server.NoRoute(func(c *gin.Context) {
+		c.JSON(404, gin.H{"code": "Not found", "message": "Page not found", "env": env.ServerConfig.EnvName})
+	})
+
+	glog.Infof("server running in %s mode", env.ServerConfig.EnvName)
+
+	routers.SetupRouter(server)
+
+	host, port := env.ServerConfig.ListenAddress, env.ServerConfig.ListenPort
+	log.Fatal(server.Run(fmt.Sprintf("%s:%d", host, port)))
+}
