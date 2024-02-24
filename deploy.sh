209f3e0864f94634d14702b7a5e2605db81c2d6e
Yashvardhan Kukreja <yash.kukreja.98@gmail.com>
2024-02-24 18:01:38 +0000

    server stop fix

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ cd FinEase/backend
 git reset --hard HEAD~0
 git pull origin release/SIT --rebase
 go build -o ~/FinEase/backend/bin/finease-backend ~/FinEase/backend/cmd/main.go
-lsof -i tcp:8000 | awk '{ print $2; }' | grep -v "PID" | xargs kill > /dev/null 2&>1
+ps aux | grep "fin" | grep -v "grep" | awk '{ print $2; }' | xargs kill > /dev/null 2&>1
 ~/FinEase/backend/bin/finease-backend serve --db-name=$DB_NAME --db-user=$DB_USER --db-password=$DB_PASSWORD --db-host=$DB_HOST --db-port=$DB_PORT &
