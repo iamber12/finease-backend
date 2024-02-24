e7e9a9dbeb7e27e1305638aaa68bbc2a461bcb92
Yashvardhan Kukreja <yash.kukreja.98@gmail.com>
2024-02-24 12:21:40 -0500

    fix: server stop fix
    Signed-off-by: Yashvardhan Kukreja <yash.kukreja.98@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
+set -o errexit
+
 cd FinEase/backend
 git pull origin release/SIT
 go build -a -o ~/FinEase/backend/bin/finease-backend ~/FinEase/backend/cmd/main.go
-make stop
+lsof -i tcp:8000 | awk '{ print $2; }' | grep -v "PID" | xargs kill > /dev/null 2&>1
 ~/FinEase/backend/bin/finease-backend serve --db-name=$DB_NAME --db-user=$DB_USER --db-password=$DB_PASSWORD --db-host=$DB_HOST --db-port=$DB_PORT &
