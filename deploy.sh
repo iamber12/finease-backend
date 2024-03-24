936eb48795ff42b3665ec6039e893e9f5cc8af8b
Amber Verma <a23verma@uwaterloo.ca>
2024-03-24 22:38:31 +0000

    deploy.sh edited online with Bitbucket

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 cd FinEase/backend
 git reset --hard HEAD~0
 git pull origin dev --rebase
-nohup go build -a -o ./bin/finease-backend ./cmd/main.go > build.log 2>&1 &
-~/FinEase/backend/bin/finease-backend serve --db-name=$DB_NAME --db-user=$DB_USER --db-password=$DB_PASSWORD --db-host=$DB_HOST --db-port=$DB_PORT &
+nohup go build -a -o ./bin/finease-backend ./cmd/main.go > build.log 2>&1
