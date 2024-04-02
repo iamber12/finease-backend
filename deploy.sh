88544782cf02396c05992602b34ca09dda492138
Amber Verma <a23verma@uwaterloo.ca>
2024-04-02 22:26:36 +0000

    deploy.sh edited online with Bitbucket

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,5 @@ sudo lsof -i :8000 | grep LISTEN | awk '{print $2}' | xargs -r kill
 cd FinEase/backend
 git reset --hard HEAD~0
 git pull origin release/SIT --rebase
-which go
-go build -o ./bin/finease-backend ./cmd/main.go
+/usr/local/go/bin/go build -o ./bin/finease-backend ./cmd/main.go
 ~/FinEase/backend/bin/finease-backend serve --db-name=$DB_NAME --db-user=$DB_USER --db-password=$DB_PASSWORD --db-host=$DB_HOST --db-port=$DB_PORT &
